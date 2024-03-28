51287abca5f19ad63e057cf18f30122134324638
digitalgenieji <digiaffair@gmail.com>
2024-03-28 14:51:41 +0530

    hwd

--- next.config.js
+++ next.config.js
@@ -1,38 +1,7 @@
-const path = require('path')
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /** 
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
-  },
-  */
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
+module.exports = {
   output: "export",
-
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
+  basePath: "/github-pages",
   images: {
     unoptimized: true,
-  },
-};
-
-export default nextConfig;
+  }
+}
