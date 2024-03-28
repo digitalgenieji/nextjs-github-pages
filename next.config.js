ded281d6a012b4273175cab0c2e2be680645f257
digitalgenieji <digiaffair@gmail.com>
2024-03-28 14:11:32 +0530

    new

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@ const path = require('path')
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /** 
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     domains: ['res.cloudinary.com', 'media.dev.to']
   },
+  */
   /**
    * Enable static exports for the App Router.
    *
